7f3445e19c9df02e852397c284c65b941caf302e
Jeremy Belcher <charlesthefourth@gmail.com>
2013-06-06 21:10:03 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Acguide::Application.config.secret_token = '61c9b1f4db90e4c924bd5d2cb9e19b10f01f17b3350f3943473f6d375b3e7304532b9e80c2784671bda8205cc72fd71c423a4605b3dc6d747e74cf076ed98143'
